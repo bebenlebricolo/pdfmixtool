06269b64943d326586625cc2440fa71a60557ab0
Milo Ivir <mail@milotype.de>
2021-09-14 07:43:55 +0000

    Translated using Weblate (Croatian)
    Currently translated at 100.0% (167 of 167 strings)
    
    Translation: PDF Mix Tool/Translations
    Translate-URL: https://hosted.weblate.org/projects/pdf-mix-tool/translations/hr/

--- languages/pdfmixtool_hr.ts
+++ languages/pdfmixtool_hr.ts
@@ -213,7 +213,7 @@
     <message>
         <location filename="../src/operations/booklet.cpp" line="46"/>
         <source>Generate two booklets in one sheet:</source>
-        <translation type="unfinished"></translation>
+        <translation>Izradi dvije knjižice na jednom listu:</translation>
     </message>
     <message>
         <location filename="../src/operations/booklet.cpp" line="54"/>
